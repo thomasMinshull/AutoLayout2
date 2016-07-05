9c263458dce599df002d0cc8f6ce045e894d17eb
Thomas Minshull <thomasminshull@gmail.com>
2016-07-05 16:14:19 -0700

    initial commit

---
+++ AutoLayout2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout2
+//
+//  Created by thomas minshull on 2016-07-05.
+//  Copyright © 2016 thomas minshull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
